5133c430f2dcd232d8103d8e452d37eefa7611a7
PonHsu <pon@mail.biotech-tw.com.tw>
2020-11-04 11:49:16 +0800

    1.新增報表-工單得率查詢

---
+++ CHPOUTSRCMES.Web/Controllers/ReportController.cs
@@ -0,0 +1,36 @@
+﻿using CHPOUTSRCMES.Web.Models.Report;
+using CHPOUTSRCMES.Web.ViewModels;
+using CHPOUTSRCMES.Web.ViewModels.Process;
+using CHPOUTSRCMES.Web.ViewModels.Report;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CHPOUTSRCMES.Web.Controllers
+{
+    public class ReportController : Controller
+    {
+        YieldQueryModel yieldQueryModel = new YieldQueryModel();
+        // GET: Report
+        public ActionResult Yield()
+        {
+            YieldViewModel yieldViewModel = new YieldViewModel();
+            ProcessViewModel processViewModel = new ProcessViewModel();
+            yieldViewModel.BathNoList = processViewModel.GetBatchNo();
+            yieldViewModel.MachineCodeList = processViewModel.GetManchine();
+            return View(yieldViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public JsonResult YieldQuery(DataTableAjaxPostViewModel data, string cuttingDateFrom, string cuttingDateTo, string batchNo, string machineNum)
+        {
+            var userId = User.Identity.GetUserId();
+            var models = yieldQueryModel.getModels(data, cuttingDateFrom, cuttingDateTo, batchNo, machineNum, userId);
+            return Json(models, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ CHPOUTSRCMES.Web/ViewModels/Report/YieldViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CHPOUTSRCMES.Web.Models.Report;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CHPOUTSRCMES.Web.ViewModels.Report
+{
+    public class YieldViewModel
+    {
+
+        [Display(Name = "裁切完工日期")]
+        public DateTime? CuttingDateFrom { set; get; }
+        public DateTime? CuttingDateTo { set; get; }
+
+        [Display(Name = "工單號")]
+        public string BatchNo { set; get; }
+
+        [Display(Name = "機台")]
+        public string MachineNum { set; get; }
+
+        public YieldQueryModel Fields { set; get; }
+
+        public IEnumerable<SelectListItem> BathNoList { set; get; }
+
+        public IEnumerable<SelectListItem> MachineCodeList { set; get; }
+    }
+}
